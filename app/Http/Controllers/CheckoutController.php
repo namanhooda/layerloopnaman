4c70dbc864bda6e0c5f9239c31cdfbc3c91a40a4
Naman Hooda <namanhooda@Namans-MacBook-Air.local>
2025-09-10 17:23:13 +0530

    nmn

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Razorpay\Api\Api; 
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
