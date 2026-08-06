caf6b7a6ce5792117c6daffefb39f8bcf5ceaad4
Naman Hooda <namanhooda@Namans-MacBook-Air.local>
2026-08-06 19:50:25 +0530

    nmn work

--- app/Http/Controllers/ShiprocketController.php
+++ app/Http/Controllers/ShiprocketController.php
@@ -35,7 +35,7 @@ public function generateToken(Request $request)
             ],
             "mobile_app" => false
         ],
-        "redirect_url" => url('/order/success'),
+        "redirect_url" => url('/checkout'),
         "timestamp" => now()->toIso8601ZuluString()
     ];
 
