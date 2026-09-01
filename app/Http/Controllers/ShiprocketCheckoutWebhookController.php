05fdae9f9c9020c53932f9624b1f93da6fc3ebd9
Naman Hooda <namanhooda@Namans-MacBook-Air.local>
2026-09-01 15:20:08 +0530

    nmn

--- app/Http/Controllers/ShiprocketCheckoutWebhookController.php
+++ app/Http/Controllers/ShiprocketCheckoutWebhookController.php
@@ -23,7 +23,7 @@ class ShiprocketCheckoutWebhookController extends Controller
     public function handle(Request $request)
     {
         $response = json_encode($request->all());
-        $orderId = $request->input('order_id');
+        $orderId = $response->cart_id;
         DB::table('tests')->insert([
             'payload' => json_encode($request->all()),
         ]);
@@ -33,7 +33,18 @@ class ShiprocketCheckoutWebhookController extends Controller
 
     public function order(Request $request, $OrderId)
     {
-        $orderId = "6a95a08b53128e4bd571fed1";
+        $orderId = $OrderId;
+
+
+        $newOrder =  Order::where('shiprocket_checkout_id', $orderId)->first();
+
+        if($newOrder){
+            return response()->json([
+                'status' => false,
+                'message' => 'Order already exists.',
+            ], 400);
+        }
+
 
         $timestamp = now()->utc()->toIso8601ZuluString();
 
